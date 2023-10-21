b3f95c211fc3fedb887fcacf949065221c2061d8
Malvere <gatling1404@gmail.com>
2023-10-21 14:57:22 +0300

    log

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from web.plat import is_docker
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 if not is_docker():
+    print("Using polling mode")
     bot = asyncio.run(start_bot())
 else:
+    print("Establishing webhook")
     bot = start_webhook()
