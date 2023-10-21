3d7d9d409ee03a4fc5bd51653698b010173a5260
Malvere <gatling1404@gmail.com>
2023-10-21 20:06:22 +0300

    prep.sh

--- prep.sh
+++ prep.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-poetry install
-playwright install firefox
-# playwright install-deps firefox
-# python main.py
+#!/usr/bin/env bash
+set -e
+
+yarn playwright install firefox
+
+# Store/pull Playwright cache with build cache
+if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH]]; then 
+  echo "...Copying Playwright Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+else 
+  echo "...Storing Playwright Cache in Build Cache" 
+  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
+fi
